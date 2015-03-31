5886295e2069648355fc44b86c4d53e7f13702dd
Adam Duren <adamduren@gmail.com>
2015-03-31 16:02:39 -0700

    Move the board template out to the directive, allow toggle between boards

--- src/components/retro-board/directive.js
+++ src/components/retro-board/directive.js
@@ -9,7 +9,11 @@
       return {
         controller: RetroBoardController,
         controllerAs: 'vm',
-        bindToController: true
+        bindToController: true,
+        templateUrl: 'components/retro-board/template.html',
+        scope: {
+          data: '='
+        }
       };
     }
 
--- src/main.js
+++ src/main.js
@@ -5,10 +5,23 @@
     .module('retro', ['retro-board'])
     .controller('MainController', MainController);
 
-    function MainController($scope, $mdSidenav) {
+    function MainController($scope, $mdSidenav, boardService) {
       $scope.toggleLeftMenu = function() {
         $mdSidenav('left').toggle();
       };
+
+      $scope.boards = [];
+
+      $scope.addBoard = function(name) {
+        $scope.boards.push({name: name});
+        $scope.newBoardName = '';
+      };
+
+      $scope.openBoard = function(board) {
+        $scope.toggleLeftMenu();
+        $scope.activeBoard = board;
+      };
+
     }
 
     MainController.$inject = ['$scope', '$mdSidenav'];
